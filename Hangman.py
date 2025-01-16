e7551f5a21215f57c8096aba57c91f8d9bc9a75b
kemverlyreyes30 <kemverlyreyes21@gmail.com>
2025-01-16 11:40:12 +0800

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -52,9 +52,40 @@ if category == 0:
 elif category == 1:
     print("You have selected, Mathematics")
     print("Try to guess the word. You have 6 incorrect guesses.")
-   
+
+  
+   words = ["calculus", "theorem", "equation", "algebra", "probability", "geometry"]
+
+    word = secrets.choice(words)  
+    guessed_word = ["_"] * len(word) 
+    attempts = 6
+    guessed_letters = set()
+
+    while attempts > 0 and "_" in guessed_word:
+        print("\nWord to guess:", " ".join(guessed_word))
+        guess = input("Guess a letter: ").lower()
+
+        if guess in guessed_letters:
+            print("You already guessed that letter. Try again.")
+        elif guess in word:
+            guessed_letters.add(guess)
+            print(f"Correct! '{guess}' is in the word.")
+            for i, letter in enumerate(word):
+                if letter == guess:
+                    guessed_word[i] = guess
+        else:
+            guessed_letters.add(guess)
+            attempts -= 1
+            print(f"Incorrect! '{guess}' is not in the word. Attempts left: {attempts}")
+
+    if "_" not in guessed_word:
+        print("\nCongratulations! You guessed the word:", word)
+    else:
+        print("\nGame over! The word was:", word
 
  
+
+
 
 
 elif category == 2:
