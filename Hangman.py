c6c810219c85c9638a614781da01a3911ce24a05
trixvergara <vergaratrix.22@gmail.com>
2025-01-16 04:41:52 -0800

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -46,11 +46,14 @@ if category == 0:
 
 
 
+
+
+
 elif category == 1:
     print("You have selected, Mathematics")
     print("Try to guess the word. You have 6 incorrect guesses.")
   
-    words = ["calculus", "theorem", "equation", "algebra", "probability", "geometry"]
+   words = ["calculus", "theorem", "equation", "algebra", "probability", "geometry"]
 
     word = secrets.choice(words)  
     guessed_word = ["_"] * len(word) 
@@ -77,7 +80,7 @@ elif category == 1:
     if "_" not in guessed_word:
         print("\nCongratulations! You guessed the word:", word)
     else:
-        print("\nGame over! The word was:", word)
+        print("\nGame over! The word was:", word
 
  
 
@@ -85,7 +88,7 @@ elif category == 1:
 
 
 elif category == 2:
-    print("You have selected, Science")
+    print("You have selected, science")
     print("Try to guess the word. You have 6 incorrect guesses.")
 
     words = ["molecule", "gravity", "chemistry", "genetics", "photosynthesis", 
@@ -126,13 +129,43 @@ elif category == 3:
     print("You have selected, English")
     print("Try to guess the word. You have 6 incorrect guesses.")
     
+words = ["grammar", "adjective", "onomatopoeia", "homophones", "prepositional", "hyperbole", "connotation"]
+
+  word = secret.choice(words) 
+    guessed_word = ["_"] * len(word) 
+    attempts = 6
+    guessed_letters = set() 
+    
+    while attempts > 0 and "_" in guessed_word: 
+        print("\nWord to guess:", " ".join(guessed_word))
+        guess = inout("Guess a letter: ").lower()
+        
+        if guess in guessed_letters: 
+            print("You already guessed that letter. 
+        elif guess in word: 
+            guessed_letters.add(guess)
+            print("You already guessed that letter. Try again.") 
+            for i, letter in enumerate(word):
+            if letter == guess:
+                guessed_word[i] = guess
+        else:
+            guessed_letters.add(guess)
+            attempts -= 1
+            print(f"Incorrect! '{guess}' is not in the word. Attempts left: {attempts}")
+    
+    if "_" not in guessed_word:
+        print("\nCongratulations! You guessed the word:", word)
+    else:
+        print("\nGame over! The word was:", word)
+        
+        
 elif category == 4:
     print("You have selected, History")
     print("Try to guess the word. You have 6 incorrect guesses.")
 
-    words = ["civilization", "colonization", "revolution", "archaeology", "imperialism"]
+words = ["civilization", "colonization", "revolution", "archaeology", "imperialism"]
 
-    word = secrets.choice(words)  
+  word = secrets.choice(words)  
     guessed_word = ["_"] * len(word) 
     attempts = 6
     guessed_letters = set()
